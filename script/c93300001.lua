590b1e5143e1371b63a9c29ea092df6409f6a0d8
Coyot23 <69750820+Coyot23@users.noreply.github.com>
2026-02-27 17:52:08 +0100

    Refactor Gift of Color Vision skill implementation

--- script/c93300001.lua
+++ script/c93300001.lua
@@ -1,26 +1,21 @@
+-- The Gift of Color Visioin
 local s,id=GetID()
-
 function s.initial_effect(c)
 	aux.AddVrainsSkillProcedure(c,s.flipcon,s.flipop,EVENT_PREDRAW)
 end
-
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0
 		and Duel.IsTurnPlayer(tp)
 		and Duel.GetDrawCount(tp)==1
 		and (Duel.IsDuelType(DUEL_1ST_TURN_DRAW) or Duel.GetTurnCount()>1)
 end
-
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,id)>1000000000000000 then return end
 	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
-
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-
 	if aux.CheckSkillNegation(e,tp) then return end
-
 	-- THIS is how you cancel the draw in EDOPro
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -30,19 +25,14 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(0)
 	e1:SetReset(RESET_PHASE+PHASE_DRAW)
 	Duel.RegisterEffect(e1,tp)
-
 	-- custom draw replacement logic
 	Duel.ConfirmDecktop(tp,1)
 	local tc=Duel.GetDecktopGroup(tp,1):GetFirst()
 	if not tc then return end
-
 	Duel.SendtoDeck(tc,nil,-2,REASON_EFFECT+REASON_RULE)
-
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
 	local ac=Duel.AnnounceCard(tp)
-
 	local token=Duel.CreateToken(tp,ac)
 	Duel.SendtoHand(token,nil,REASON_EFFECT)
 	Duel.ConfirmCards(1-tp,token)
 end
-
